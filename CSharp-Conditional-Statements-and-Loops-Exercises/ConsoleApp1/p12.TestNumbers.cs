fd3a30824e210d8c0a163ce22cca4c8e3f7dd2b7
stefankon <st.konstantinov72@gmail.com>
2017-06-03 10:16:47 +0300

    Added exercises on 02.06.2017.

---
+++ CSharp-Conditional-Statements-and-Loops-Exercises/ConsoleApp1/p12.TestNumbers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+            var maximumSum = int.Parse(Console.ReadLine());
+
+            var totalSum = 0;
+            var counter = 0;
+
+
+            for (int firstDigit = firstNumber; firstDigit >= 1; firstDigit--)
+            {
+
+                for (int secondDigit = 1; secondDigit <= secondNumber; secondDigit++)
+                {
+                    if (totalSum > maximumSum)
+                    {
+                        break;
+                    }
+
+                    //Console.Write($"{firstDigit} {secondDigit}; ");
+                    totalSum = totalSum + (3 * (firstDigit * secondDigit));
+                    counter++;
+
+                }
+
+            }
+            if (totalSum >= maximumSum)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{counter} combinations");
+                Console.WriteLine($"Sum: {totalSum} >= {maximumSum}");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{counter} combinations");
+                Console.WriteLine($"Sum: {totalSum}");
+            }
+            
+        }
+    }
+}
---
+++ CSharp-Conditional-Statements-and-Loops-Exercises/ConsoleApp2/p13.GameOfNumbers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+            var magicalNumber = int.Parse(Console.ReadLine());
+
+            var lastFirstNumber = 0;
+            var lastSecondNumber = 0;
+            var counter = 0;
+  
+
+
+            for (int firstDigit = firstNumber; firstDigit <= secondNumber; firstDigit++)
+            {
+
+                for (int secondDigit = firstNumber; secondDigit <= secondNumber; secondDigit++)
+                {
+                   
+
+                    if ((firstDigit + secondDigit) == magicalNumber)
+                    {
+                        lastFirstNumber = firstDigit;
+                        lastSecondNumber = secondDigit;
+
+
+                    }
+
+
+                    //Console.Write($"{firstDigit} {secondDigit}; ");
+                    //Console.Write($"{lastFirstNumber} {lastSecondNumber}; ");
+
+                    counter++;
+                }
+
+               
+            }
+
+            
+            if ((lastFirstNumber + lastSecondNumber) == 0)
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {magicalNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Number found! {0} + {1} = {2}", lastFirstNumber, lastSecondNumber, 
+                    lastFirstNumber + lastSecondNumber);
+                //Console.WriteLine($"Sum: {totalSum}");
+            }
+
+        }
+    }
+}
