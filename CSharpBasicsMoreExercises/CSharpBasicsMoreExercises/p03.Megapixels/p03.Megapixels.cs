cea22681de7019f7d83cb9aabdfb8e048b9feae7
stefankon <st.konstantinov72@gmail.com>
2017-06-04 23:15:40 +0300

    Added issues from CSharp Basics More Exercises.

---
+++ CSharpBasicsMoreExercises/CSharpBasicsMoreExercises/p03.Megapixels/p03.Megapixels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p03.Megapixels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            decimal megapixels = (decimal)(width * height) / 1000000;
+            decimal result = Math.Round(megapixels, 1);
+
+            Console.WriteLine($"{width}x{height} => {result}MP");
+        }
+    }
+}
---
+++ CSharpBasicsMoreExercises/CSharpBasicsMoreExercises/p04.PhotoGallery/p04.PhotoGallery.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p04.PhotoGallery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int photoNumber = int.Parse(Console.ReadLine());
+            int day = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            decimal sizeBytes = int.Parse(Console.ReadLine());
+            int widthPixels = int.Parse(Console.ReadLine());
+            int heightPixels = int.Parse(Console.ReadLine());
+            string bytes = String.Empty;
+            string orientation = String.Empty;
+        
+            if (sizeBytes < 1000)
+            {
+                sizeBytes = (int)sizeBytes;
+                bytes = "B";
+            }
+            else if ((1000 <= sizeBytes) && (sizeBytes < 1000000))
+            {
+                sizeBytes = (int)sizeBytes / 1000;
+                bytes = "KB";
+            }
+            else
+            {
+
+                sizeBytes = Math.Round((sizeBytes / 1000000), 1);
+                bytes = "MB";
+            }
+
+            if (widthPixels < heightPixels)
+            {
+                orientation = "portrait";
+            }
+            else if (widthPixels > heightPixels)
+            {
+                orientation = "landscape";
+            }
+            else
+            {
+                orientation = "square";
+            }
+
+            Console.WriteLine($"Name: DSC_{photoNumber:D4}.jpg");
+            Console.WriteLine($"Date Taken: {day:D2}/{month:D2}/{year} {hours:D2}:{minutes:D2}");
+            Console.WriteLine($"Size: {sizeBytes}{bytes}");
+            Console.WriteLine($"Resolution: {widthPixels}x{heightPixels} ({orientation})");
+        }
+    }
+}
