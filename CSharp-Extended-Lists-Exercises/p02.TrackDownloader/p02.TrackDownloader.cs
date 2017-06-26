ff1a59222776a5cad3672bc35de61bbf99fdd0af
stefankon <st.konstantinov72@gmail.com>
2017-06-26 22:23:17 +0300

    New

--- CSharp-Extended-Lists-Exercises/p02.TrackDownloader/p02.TrackDownloader.cs
+++ CSharp-Extended-Lists-Exercises/p02.TrackDownloader/p02.TrackDownloader.cs
@@ -10,6 +10,7 @@ namespace p02.TrackDownloader
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             List<string> checkList = Console.ReadLine().Split(' ').ToList();
             string currentInput = Console.ReadLine();
             
@@ -34,8 +35,43 @@ namespace p02.TrackDownloader
 
             }
             playList.Sort();
+=======
+            string currentInput = Console.ReadLine();
+            string exception1 = "Metallica";
+            string exception2 = "Linkin Park";
+
+            List<string> playList = new List<string>();
+            List<string> checkList = new List<string>();
+            if (currentInput == "end")
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                while (currentInput != "end")
+                {
+                    bool exceptions = checkList.Contains(exception1) || currentInput.Contains(exception2);
+
+                    checkList = currentInput.Split(' ').ToList();
+
+                    for (int cnt = 0; cnt < checkList.Count; cnt++)
+                    {
+                        if (!exceptions && currentInput != "end")
+                        {
+                            playList.Add(currentInput);
+                        }
+                        currentInput = Console.ReadLine();
+                    }
+
+                }
+            }
+           
+>>>>>>> 6146da216cc14913ba18fc89db7dae39bdeb7307
             Console.WriteLine(String.Join("\n", playList));
         }
     }
 }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 6146da216cc14913ba18fc89db7dae39bdeb7307
