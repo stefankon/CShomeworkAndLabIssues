d8f833e63f760d0f78106524ff9bc8b6052da57b
stefankon <st.konstantinov72@gmail.com>
2017-07-27 07:48:51 +0300

    Added new issues.

---
+++ CSharp-Objects-and-Simple-Classes/CSharp-Objects-and-Simple-Classes-Lab/p04.DistancePoints/DistancePoints.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p04.DistancePoints
+{
+    class DistancePoints
+    {
+        static void Main(string[] args)
+        {
+            Point firstPoint = Point.ParsePoint(Console.ReadLine());
+            Point secondPoint = Point.ParsePoint(Console.ReadLine());
+
+            double distance = CalcDistance(firstPoint, secondPoint);
+        }
+
+        private static double CalcDistance(Point firstPoint, Point secondPoint)
+        {
+            double sideA = Math.Pow((firstPoint.X - secondPoint.X), 2);
+            double sideB = Math.Pow((firstPoint.Y - secondPoint.Y), 2);
+
+
+        }
+    }
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int xCoord, int yCoord)
+        {
+            this.X = xCoord;
+            this.Y = yCoord;
+        }
+        public static Point ParsePoint(string input)
+        {
+            int[] inputData = input.Split(' ').Select(int.Parse).ToArray();
+            return new Point(inputData[0], inputData[1]);
+        }
+    }
+}
